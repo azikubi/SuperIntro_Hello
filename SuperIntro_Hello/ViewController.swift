555cd7e9c0c8da164cb47833dd22a038e74fe291
azikubi <k.arikuni@me.com>
2016-06-26 08:53:17 +0900

    Initial Commit

---
+++ SuperIntro_Hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperIntro_Hello
+//
+//  Created by 有國公裕 on 2016/06/26.
+//  Copyright © 2016年 js3foo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
